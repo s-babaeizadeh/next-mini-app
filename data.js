82e642156b788e5b0b3a0b74d2c9509f1ad6774c
Soudeh Babaeizadeh <babaeizadeh.s@gmail.com>
2021-05-11 09:49:08 +0430

    first version

---
+++ data.js
@@ -0,0 +1,31 @@
+export const articles = [
+  {
+    id: "1",
+    title:
+      "JavaScript (JS) is a lightweight, interpreted, programming language",
+    excerpt:
+      "just-in-time compiled programming language with first-class functions.",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quos fugiat eveniet, voluptatum nemo modi molestias veniam quasi iusto. Illum illo repudiandae nesciunt doloribus eligendi culpa nihil nostrum iure ab.",
+  },
+  {
+    id: "2",
+    title: "Introducing JavaScript objects?",
+    excerpt:
+      "In JavaScript, most things are objects, from core JavaScript features like arrays to the browser APIs built on top of JavaScript.",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut quas voluptatem iusto, porro sed, unde praesentium laborum quos nisi delectus voluptate sunt aliquam quibusdam corporis natus laudantium numquam minima. Dolorum unde laudantium labore alias aliquid provident quasi id odio.",
+  },
+  {
+    id: "3",
+    title: "Object-oriented JavaScript",
+    excerpt:
+      "With the basics out of the way, we'll now focus on object-oriented JavaScript (OOJS)",
+    body: "There are many things to consider when setting up a website, and accessibility is one factor that can sometimes be overlooked. An accessible website is one that’s optimized for all people, including those with impaired vision or hearing, motor difficulties, or learning disabilities",
+  },
+  {
+    id: "4",
+    title: "Inheritance in JavaScript",
+    excerpt:
+      "This article shows how to create child object classes (constructors) that inherit features from their parent classes",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur aut quas voluptatem iusto, porro sed, unde praesentium laborum quos nisi delectus voluptate sunt aliquam quibusdam corporis natus laudantium numquam minima. Dolorum unde laudantium labore alias aliquid provident quasi id odio.",
+  },
+]
---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Meta from "../components/Meta"
+
+const about = () => {
+  return (
+    <div>
+      <Meta title="About" />
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum magnam
+        in deleniti impedit incidunt cupiditate ut voluptatem harum consequuntur
+        accusamus!
+      </p>
+    </div>
+  )
+}
+
+export default about
