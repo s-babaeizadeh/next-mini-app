d139855c438156e8d6f51413adc6b02eab873140
Soudeh Babaeizadeh <babaeizadeh.s@gmail.com>
2023-09-08 22:33:42 +0330

    second versrion

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,18 @@
-import Meta from "../components/Meta"
+import Meta from '../components/Meta';
+import navStyles from '../styles/About.module.css';
 
 const about = () => {
   return (
     <div>
       <Meta title="About" />
       <h1>About</h1>
-      <p>
+      <p className={navStyles.about}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum magnam
         in deleniti impedit incidunt cupiditate ut voluptatem harum consequuntur
         accusamus!
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default about
+export default about;
